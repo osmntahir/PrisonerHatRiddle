282c8c14eccc99d881cc1d0b4f5c42299cb65436
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2023-03-20 13:05:27 +0300

    update to optimize

--- src/hatRiddle.java
+++ src/hatRiddle.java
@@ -1,7 +1,9 @@
+
 public class hatRiddle {
     private String[] peopleHats = new String[10];
     private String[] predictedHats = new String[10];
     private String[] hats = new String[2];
+
 
     public hatRiddle() {
         hats[0] = "beyaz";
@@ -12,91 +14,34 @@ public class hatRiddle {
         }
     }
 
-    void guess() {
-        // simdiye kadar ki tahmin edilen siyah sapka sayisi
-        int totalblackhats = 0;
-        // insanlarin onundeki siyah sapka sayisi
-        int nextnumberofBlackHats=0;
-
-        for (int i =0 ; i <10;i++)
-        {
-            for (int j=i+1; j<10;j++)
-            {
-                if (peopleHats[j].equals(hats[1]))
-                {
-                    nextnumberofBlackHats++;
-                }
+    void guess()
+    {
+        int[] blackHatCnts = new int[10];
+        int blackHatCntInIndex = 0;
+        for (int i = 10-1; i > 0; i--) {
+            if(peopleHats[i].equals(hats[1])) {
+                blackHatCntInIndex++;
             }
-            // ilk eleman
-            if (i==0)
-            {
-                if ((nextnumberofBlackHats)%2==0)
-                {
-                    predictedHats[0]=hats[0];
-                }
-                else {
-                    predictedHats[0]=hats[1];
-                }
-            }
-            // son eleman
-            else if(i==9)
-            {
-                if (predictedHats[0].equals(hats[1]))
-                {
-                    if (totalblackhats%2==0)
-                    {
-                        predictedHats[i]=hats[1];
-                    }
-                    else
-                    {
-                        predictedHats[i]=hats[0];
-                    }
-                }
-                else
-                {
-                    if (totalblackhats%2==0)
-                    {
-                        predictedHats[i]=hats[0];
-                    }
-                    else
-                    {
-                        predictedHats[i]=hats[1];
-                    }
-                }
-            }
-            // ara durum
-            else
-            {
-                if (predictedHats[0].equals(hats[1]))
-                {
-                    if ((nextnumberofBlackHats+totalblackhats)%2==0)
-                    {
-                        predictedHats[i]=hats[1];
-                        totalblackhats++;
-                    }
-                    else
-                    {
-                        predictedHats[i]=hats[0];
-                    }
-                }
-                else
-                {
-                    if ((nextnumberofBlackHats+totalblackhats)%2==0)
-                    {
-                        predictedHats[i]=hats[0];
-                    }
-                    else
-                    {
-                        predictedHats[i]=hats[1];
-                        totalblackhats++;
-                    }
-                }
-            }
-            nextnumberofBlackHats=0;
-
+            blackHatCnts[i-1] = blackHatCntInIndex;
         }
 
-       
+        // Predicting by using the counts
+        boolean isEven = false;
+        for (int i = 0; i < blackHatCnts.length; i++) {
+            int blackHatCnt = blackHatCnts[i];
+            if(i == 0) {
+                predictedHats[i] = hats[(int) (Math.random() * 2)];
+            } else {
+                if((blackHatCnt % 2 == 0) && isEven) {
+                    predictedHats[i] = hats[0];
+                } else if(!(blackHatCnt % 2 == 0) && !isEven) {
+                    predictedHats[i] = hats[0];
+                } else {
+                    predictedHats[i] = hats[1];
+                }
+            }
+            isEven = blackHatCnt % 2 == 0;
+        }
     }
     public void print()
     {
@@ -110,8 +55,6 @@ public class hatRiddle {
         {
             System.out.print(predictedHats[i] + "  ");
         }
-
     }
-
 }
 
